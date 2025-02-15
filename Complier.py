e33c3b960dd3674d20424a178b0f3cf6b47be92d
Isra <Israchokwatana@gmail.com>
2025-02-15 21:55:53 +0700

    NLP update -> issue with models

--- Complier.py
+++ Complier.py
@@ -1,13 +1,16 @@
 # First, import the required lib
 import pandas as pd
-from fuzzywuzzy import fuzz
+import spacy
 
 # Step 1: import the two excel file - input file and reference file
 df_main = pd.read_excel('Excel_file/Main.xlsx')
 df_compare = pd.read_excel('Excel_file/Compare.xlsx')
 
+# Create spacy nlp object -> load en_core_web_md (small model), en_core_web_lg (large model)
+nlp = spacy.load("en_core_web_trf")
+
 # Create similarity function
-def find_match(statement, main_df, threshold = 70):
+def find_match(statement, main_df, threshold = 0):
     """
     Find if statement string is located in main_df and return statement else return none
     Args:
@@ -16,12 +19,19 @@ def find_match(statement, main_df, threshold = 70):
     Returns:
         tuple (document reference, matched statement, similarity score)
     """
+    # Process the input statement into a spacy doc
+    doc_input = nlp(statement)
+
     # Find Best Match -> Score (debugging), Document (actual use), Statement (debugging)
     best_score = 0
     best_document = None
     best_statement = None
     for _, row in main_df.iterrows(): # iterrow output a tuple (index, row)
-        score = fuzz.ratio(statement, row['Statement'])
+        # Process the statement into a spacy doc
+        doc_main = nlp(row['Statement'])
+        # Calculate similarity
+        score = doc_input.similarity(doc_main)
+        # Update best match
         if score > best_score:
             best_score = score
             best_document = row['Document']
@@ -52,4 +62,4 @@ output_df = pd.DataFrame(Result)
 print(output_df)
 
 # Step 4: Save to result excel output file
-output_df.to_excel('Excel_file/Result.xlsx', index=False)
+#output_df.to_excel('Excel_file/Result.xlsx', index=False)
--- README.md
+++ README.md
@@ -4,3 +4,14 @@ This repo represent a basic implementation of NLP processing for a comparrsion b
 
 A similarity check will be ran between inputted file and base file to find similarity between TOR points
 
+## Requirements
+
+- Python 3.7+
+- pip install pandas
+- pip install openpyxl
+- pip install spacy
+- python -m spacy download en_core_web_md
+
+## Usage
+
+- Run `python Complier.py`
